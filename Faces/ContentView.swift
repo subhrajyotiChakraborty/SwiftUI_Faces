d6f458d64d7fa47ee956c1c9e809808bbaf8ed28
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-05 13:33:56 +0530

    Initial Commit

---
+++ Faces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Faces
+//
+//  Created by Subhrajyoti Chakraborty on 05/09/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
